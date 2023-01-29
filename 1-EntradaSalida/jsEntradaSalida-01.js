bd50ffe68ed1c62e20da5da0957cba24f394cafc
SebastianCanoforever <ezequielcano205@gmail.com>
2023-01-29 15:25:11 -0300

    nuevo a mi cambio test

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,7 @@
 /*
-Al presionar el  botón, 
+Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Hola a mi mundo");
 }
-
